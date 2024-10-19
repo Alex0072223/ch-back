a6748d19e4325b71284988046f78d553d0eb9394
alex <alex0072223@mail.ru>
2024-10-20 01:44:09 +0400

    5-6

--- src/ru/grig/charm/back/ChBackRunner.java
+++ src/ru/grig/charm/back/ChBackRunner.java
@@ -3,25 +3,36 @@ package ru.grig.charm.back;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.HttpCookie;
 import java.net.Socket;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ChBackRunner {
-    public static void main(String[] args) throws IOException {
-        try (Socket socket = new Socket("localhost", 8080);
-             DataOutputStream requestStream = new DataOutputStream(socket.getOutputStream());
-             DataInputStream responseStream = new DataInputStream(socket.getInputStream());
-             Scanner scanner = new Scanner(System.in)
-        ) {
-            while (scanner.hasNextLine()){
-                String request = scanner.nextLine();
-                requestStream.writeUTF(request);
-                String response = responseStream.readUTF();
-                System.out.println("Server: "+new String(response));
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        try(HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();) {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/profile?id=1"))
+                    .GET()
+                    .build();
 
 
+            HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+            HttpClient.Version version = response.version();
+            int statusCode = response.statusCode();
+            String headers = response.headers().map().entrySet().stream().map(Object::toString).collect(Collectors.joining("\n"));
+            String body = new String(response.body());
+            System.out.println(version + " " + statusCode + "\n" + headers + "\n\n" + body);
 
-            }
         }
+
     }
 }
